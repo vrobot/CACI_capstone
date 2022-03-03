c325eb7a4cdf98eb8c9a56ab38367ee4914373f5
oaklandmurphy <aidan@niallmurphy.net>
2022-03-03 11:05:51 -0800

    Add files via upload

--- multilateration/Multilateration.py
+++ multilateration/Multilateration.py
@@ -1,11 +1,17 @@
 import numpy as np
 import sympy as sp
+import scipy
 import matplotlib.pyplot as plt
 import random
 import math
 
+from itertools import combinations
+
 from sympy import Eq, solve_linear_system, Matrix
 from sympy.interactive import printing
+
+from scipy.optimize import least_squares
+
 printing.init_printing(use_unicode=True)
 
 # Random Seed
@@ -39,7 +45,9 @@ def test_MLZ(iterations, nodes, display):
 		
 		# see if an error get generated
 		try:
-			output.append(MLZ(nodes, times, s_o_s))
+			#output.append(MLZ(nodes, times, s_o_s))
+			output.append(noisy_MLZ(nodes, times, s_o_s, 0.0000000001))
+			#output.append(noisy_TRI(nodes, times, s_o_s, 0.0001))
 			print("completed equation ", (i+1), " of ", iterations)
 
 		# increment wrong guesses if an error occurs
@@ -63,7 +71,7 @@ def test_MLZ(iterations, nodes, display):
 
 			incorrect_guesses += 1
 
-		# display the guesses and sound sources, display the 
+		# display the guesses and sound sources
 		if display == True & correct_show == True:
 			
 			#plot guess
@@ -131,7 +139,19 @@ def MLZ(nodes, times, v):
 
 	return result_numeric #, result_symbolic  <-------------------------- uncomment for symbolic solutions
 
+def noisy_MLZ(nodes, times, v, error_scale):
+	#convert the scale of an error to its absolute value
+	error_abs = error_scale * find_farthest_points(nodes)
 
+	# create a normal distribution based on the error scale and apply it to each node's timestamp
+	for i, t in enumerate(times):
+		print('before: ', times[i])
+		times[i] = t + np.random.normal(0, error_abs)
+		print('after:  ', times[i])
+
+
+	output = MLZ(nodes, times, v)
+	return output
 
 # helper function to calcualte times that sound will arrive at each node for testing
 # nodes is formatted as follows: [[x1, y1], [x2, y2], [x3, y3] ... [xn, yn]]
@@ -144,26 +164,129 @@ def calcTimes (Sx, Sy, nodes, v):
 	return times
 
 
-'''
-nodeA_time, nodeB_time, nodeC_time = calcTimes(source_x, source_y, nodeA_x, nodeA_y, nodeB_x, nodeB_y, nodeC_x, nodeC_y, speed_of_sound) 
+def find_farthest_points(points):
+	
+	distances = []
 
-guess_n, guess_s = MLZ(nodeA_x, nodeA_y, nodeA_time,    nodeB_x, nodeB_y, nodeB_time,    nodeC_x, nodeC_y, nodeC_time,    speed_of_sound)
+	for combo in combinations(points, 2):  # 2 for pairs
+		distances.append(math.dist(combo[0], combo[1]))
+	
+	return max(distances)
+
+def LST(nodes, times, v):
+
+	def equations(guess):
+			x, y, r = guess
+
+			output = []
+
+			for i, n in enumerate(nodes):
+				x1 = n[0]
+				y1 = n[1]
+				dist1 = v * times[i]
+				output.append((x - x1)**2 + (y - y1)**2 - (dist1 - r)**2)
+
+			return tuple(output)
+
+	mean_x = np.mean([row[0] for row in nodes])
+	mean_y = np.mean([row[1] for row in nodes])
+
+	inital_guess = (mean_x, mean_y, 0)
+
+	final_guess = least_squares(equations, inital_guess)
+	print(final_guess)
+
+	return [final_guess.x[0], final_guess.x[1]]
+
+def noisy_LST(nodes, times, v, error_scale):
+	#convert the scale of an error to its absolute value
+	error_abs = error_scale * find_farthest_points(nodes)
+
+	# create a normal distribution based on the error scale and apply it to each node's timestamp
+	for i, t in enumerate(times):
+		print('before: ', times[i])
+		times[i] = t + np.random.normal(0, error_abs)
+		print('after:  ', times[i])
+
+	output = LST(nodes, times, v)
+	return output
+
+def test_LST(iterations, nodes, display, error_scale):
+	
+	# speed of sound
+	s_o_s = 1
+
+	#calculate the middle of the triangle
+	mean_x = np.mean([row[0] for row in nodes])
+	mean_y = np.mean([row[1] for row in nodes])
+
+	# calculate the standard deviation from the center of the triangle
+	std_x = np.std([row[0] for row in nodes])
+	std_y = np.std([row[1] for row in nodes])
+
+	# generate a guassian distro of guesses
+	s_x = np.random.normal(mean_x, std_x, iterations)
+	s_y = np.random.normal(mean_y, std_y, iterations)
+
+	# find the farthest nodes from each other and their distance
+	checker_err = find_farthest_points(nodes) * error_scale * 2
+
+	output = []
+	incorrect_guesses = 0
+
+	# loop through each sonic source
+	for i in range(iterations):
+
+		times = calcTimes(s_x[i], s_y[i], nodes, s_o_s) 
+
+		# see if an error get generated
+		#output.append(LST(nodes, times, s_o_s))
+		output.append(noisy_LST(nodes, times, s_o_s, error_scale))
+
+		print("completed equation ", (i+1), " of ", iterations)
+
+	# for each prediction, check if the output is correct
+	for i, check in enumerate(output):
+		
+		correct_show = True
+
+		#check for wrong guesses
+		if not (math.isclose(s_x[i], check[0], rel_tol=0.0, abs_tol= checker_err) & math.isclose(s_y[i], check[1], rel_tol=0.0, abs_tol= checker_err)):
+			
+			print("incorrect guess:")
+			print("real : ", s_x[i], ", ", s_y[i])
+			print("guess: ", check[0], ", ", check[1])
+
+			correct_show = False
+
+			incorrect_guesses += 1
+
+		#if the display graph setting is on then add current node to plot of guesses and answers
+		if(display == True):
+			#plot the real location of the sound
+			plt.scatter(s_x[i], s_y[i], color = 'grey', marker='o')
+
+			# display the guesses and sound sources
+			if correct_show == True:
+				#plot right guess
+				plt.scatter(check[0], check[1], color = 'green', marker='x')
+
+			else:
+				#plot wrong guess
+				plt.scatter(check[0], check[1], color = 'red', marker='x')
 
 
-# plot all nodes, the sound source, and each prediciton
-# Black dots are microphones, red Dots are sonic origin points and blue x's are predictions
-plt.scatter(nodeA_x, nodeA_y, color = 'black')
-plt.scatter(nodeB_x, nodeB_y, color = 'black', marker='^')
-plt.scatter(nodeC_x, nodeC_y, color = 'black', marker='s')
-plt.scatter(source_x, source_y, color = 'red', marker='o')
-for i in guess_n:
-	plt.scatter(guess_n[0], guess_n[1], color = 'blue', marker='x')
+			#display listening nodes
+			for i, point in enumerate(nodes):
+				mark = '$' + str(i) + '$'
+				plt.scatter(point[0], point[1], color = 'black', marker=mark)
+		
+		#show the whole plot
+	if(display == True):
+		plt.show()
 
-for i in range(len(guess_s)):
-	plt.scatter(guess_s[i][0], guess_s[i][1], color = 'green', marker='+')
-
-plt.show()
-'''
+	# return proportion of correct guesses
+	return (iterations - incorrect_guesses) / iterations
 
 # change variables here
 speed_of_sound = 1
@@ -178,8 +301,12 @@ iterations = 100
 # set whether or not a graph of outputs is shown
 display_results = True
 
+# set the size of noise error with regard to maximum distance between nodes
+error_scale = 0.01
+
 #display the accuracy of the test
-accuracy = test_MLZ(iterations, node_list, display_results)
+accuracy = test_LST(iterations, node_list, display_results, error_scale)
+
 print("Accuracy: ", (accuracy*100), '%')
 
 print('done!')
